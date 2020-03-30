3004cf0d78beec79be2d929b3f8dc0fdf0a93b28
Daniil Koshelev <dkoshelev405@gmail.com>
2020-03-30 12:42:54 +0300

    Day start 30 march

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(User::all());
+        UserResource::collection(User::all());
     }
 
     /**
