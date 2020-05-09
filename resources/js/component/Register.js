9583eedc26f9ccf66627513d1350a995e8e4f56b
GunsStarR <vadim.eremin.9v@gmail.com>
2020-05-09 21:41:47 +0300

    front register add fields

--- resources/js/component/Register.js
+++ resources/js/component/Register.js
@@ -5,24 +5,50 @@ export default class Register extends React.Component {
     super(props);
 
     this.handleChangeLogin = this.handleChangeLogin.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangePasswordConfirmation = this.handleChangePasswordConfirmation.bind(this);
     this.handleClickBack = this.handleClickBack.bind(this);
     this.handleClickRegister = this.handleClickRegister.bind(this);
 
-    this.state = {login: ""};
+    this.state = {login: "", password: "", password_confirmation: ""};
   }
 
   reqsValidate() {
-    const {login} = this.state;
+    const { login, password, password_confirmation } = this.state;
 
-    if (login.length != 6 || /[^0-9]/.test(login))
+    if (login.length != 6 || /[^0-9]/.test(login)) {
+      document.querySelector(".register__error").textContent = "Неверный формат логина";
       return false;
+    }
 
+    if (password.length < 6) {
+      document.querySelector(".register__error").textContent = "Неверный формат пароля";
+      return false;
+    }
+
+    if (password !== password_confirmation) {
+      document.querySelector(".register__error").textContent = "Пароли не совпадают";
+      return false;
+    }
+    
     return true;
   }
 
   handleChangeLogin(e) {
     this.setState({
       login: e.target.value
+    });
+  }
+
+  handleChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  handleChangePasswordConfirmation(e) {
+    this.setState({
+      password_confirmation: e.target.value
     });
   }
 
@@ -59,6 +85,14 @@ export default class Register extends React.Component {
         <input className="auth__field" type="number" placeholder="Номер ИСУ" 
           onChange={this.handleChangeLogin}
           value={this.state.login}
+        />
+        <input className="auth__field" type="text" placeholder="Пароль" 
+           onChange={this.handleChangePassword}
+           value={this.state.password}
+        />
+        <input className="auth__field" type="text" placeholder="Подтвержение пароля" 
+           onChange={this.handleChangePasswordConfirmation}
+           value={this.state.password_confirmation}
         />
         <p className="auth__error register__error"></p>
         <div className="auth__btns">
